81f1c8c49eee0557cfa8c8ea3824a63dd0c85d59
DavidIncarbone <david.incarbone@gmail.com>
2025-03-25 11:50:35 +0100

    create table with all columns

--- database/migrations/2025_03_25_101355_create_trains_table.php
+++ database/migrations/2025_03_25_101355_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("agency",50);
+            $table->string("departure_station",255);
+            $table->string("arrival_station",255);
+            $table->time("departure_time");
+            $table->time("arrival_time");
+            $table->string("train_code",50)->unique();
+            $table->smallInteger("total_carriages")->unsigned()->nullable();
+            $table->boolean("on_time",50)->nullable();
+            $table->boolean("deleted",50);
             $table->timestamps();
         });
     }
